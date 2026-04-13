7369c3ee353f7b6270995ed37fc1e0a792ab13f7
Your Name <you@example.com>
2026-04-13 11:18:08 -0300

    atualizando codigo main

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
 export default function Home() {
   return (
-    <h1>Pagina principal</h1>
+    <main>
+      <div className="max-w-x1 space-y-6">
+          <h3 className="text-sm uppercase tracking-[0.2em] text-lime-400 font-mono">DESENVOLVEDOR FULL STACK</h3>
+          <h1 className="text-5xl md:text-7xl font-serif leading-tight tracking-tight">Gustavo Rodrigues</h1>
+          <p className="text-zinc-400 text-lg leading-relaxed max-w-md">Construo produtos web do zero — 
+            desde a API até a interface. 
+            Foco em código limpo, 
+            UX que faz sentido e entregas que não travam.</p>
+      </div>
+      <div>
+        imagem
+      </div>
+    </main>
   );
 }
